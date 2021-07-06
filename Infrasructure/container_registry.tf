20d975b1a03480f07d8c0082f22fdb638fb9252a
Raphael Bickel <rabickel@microsoft.com>
2021-07-06 14:34:49 +0200

    initial commit

---
+++ Infrasructure/container_registry.tf
@@ -0,0 +1,13 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rabickel-masteraks"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                     = "rabickel-masteraks"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                      = "Premium"
+  admin_enabled            = false
+  georeplication_locations = ["North Europe", "West Europe"]
+}
