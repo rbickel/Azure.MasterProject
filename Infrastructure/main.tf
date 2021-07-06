539111823726874d3a02463c66d8d778b4bb6f99
Raphael Bickel <rabickel@microsoft.com>
2021-07-06 17:40:37 +0200

    add terraform state

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -8,6 +8,13 @@ terraform {
   }
 
   required_version = ">= 0.14.9"
+
+  backend "azurerm" {
+    resource_group_name  = "terraform"
+    storage_account_name = "rbickeltfstate"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }  
 }
 
 provider "azurerm" {
