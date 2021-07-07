0583338bb52d193b3f4ecef3cd05c077cb1019ef
Raphael Bickel <rabickel@microsoft.com>
2021-07-07 15:07:25 +0200

    aks outputs

--- Infrastructure/aks_cluster.tf
+++ Infrastructure/aks_cluster.tf
@@ -38,6 +38,13 @@ output "client_certificate" {
   value = azurerm_kubernetes_cluster.rabickelcluster.kube_config.0.client_certificate
 }
 
+output "fqdn" {
+  value = azurerm_kubernetes_cluster.rabickelcluster.fqdn
+}
+output "private_fqdn" {
+  value = azurerm_kubernetes_cluster.rabickelcluster.private_fqdn
+}
+
 output "kube_config" {
   value     = azurerm_kubernetes_cluster.rabickelcluster.kube_config_raw
   sensitive = true
